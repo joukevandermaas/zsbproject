0dcf79a4b5232311a58aecf500129f94c244c9ca
Wessel Klijnsma <wesselklijnsma@gmail.com>
2012-06-27 10:58:55 +0200

    move

--- move.py
+++ move.py
@@ -2,16 +2,16 @@ import config
 import sys
 
 def main(eff, speed, file):
-    motion = config.loadProxy
+    motion = config.loadProxy("ALMotion")
     config.stiffnessOn(motion)
 
     f = open(file, 'r')
     d = f.readlines()
 
     for line in d:
-        mp.setAngles(eff, eval(line), speed)
+        motion.setAngles(eff, eval(line), speed)
 
-file = sys.arv[1]
+file = sys.argv[1]
 effector = sys.argv[2]
 speed = float(sys.argv[3])
 if __name__ == "__main__":
